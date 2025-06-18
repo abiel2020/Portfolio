ed9179bcdafaf174d6417ed8f8837975797eda73
Abiel Yemane <amy9270@g.rit.edu>
2025-06-18 01:16:25 -0400

    resloving build errors

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,7 +3,7 @@ import { Hero } from "@/components/hero"
 import { About } from "@/components/about"
 import { Skills } from "@/components/skills"
 import { Projects } from "@/components/projects"
-import { Contact } from "@/components/contact"
+// import { Contact } from "@/components/contact"
 
 export default function Home() {
   return (
